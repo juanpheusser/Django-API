3ca652fe833692a58bb4b6d5874f9a8d627a0f86
Juan Pablo Heusser <juanp.heusser@gmail.com>
2023-04-03 08:05:00 -0400

    Fixed security issue

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+from decouple import config
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fnap@5*v84c2mjbd!wrlm%yy_wjj$_bw_w&a0j=&b#u^0x(dx!'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
