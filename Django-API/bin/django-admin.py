1471ee681e4576fc557fa40c4f0eee161c034740
Juan Pablo Heusser <juanp.heusser@gmail.com>
2023-04-03 07:14:48 -0400

    Created Django project and app

---
+++ Django-API/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/juanpabloheusser/Documents/VS-Code/Django-API/Django-API/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
