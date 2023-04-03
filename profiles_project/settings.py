8c764e44e8a05db8cd2ddff3f8e4e1c0a930db0f
Juan Pablo Heusser <juanp.heusser@gmail.com>
2023-04-03 08:12:17 -0400

    Fixed security issue

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,6 @@ from decouple import config
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
-SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
